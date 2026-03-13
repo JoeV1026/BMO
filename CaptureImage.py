dc1773a2ea02d485a367f27be06e2054b02bfd48
Joseph Velasquez <josephvelasquez1026work@gmail.com>
2026-03-13 03:58:38 -0400

    Modified CaptureImage.py into a Tool

--- CaptureImage.py
+++ CaptureImage.py
@@ -1,39 +1,25 @@
 import cv2 as cv
-from mtcnn import MTCNN
+from FaceDetector import FaceDetector
 
 cap = cv.VideoCapture(0)
-detector = MTCNN()
+detector = FaceDetector()
 
 if not cap.isOpened():
-        print("Can't open camera")
-        exit()
+    print("Could not open camera")
+    exit()
 
 while True:
     ret, frame = cap.read()
-    
-    if not ret:
-        print("Can't see")
-        break
-    
-    flippedFrame = cv.flip(frame, 1)
-    rgb = cv.cvtColor(flippedFrame, cv.COLOR_BGR2RGB)
-    try:
-        faces = detector.detect_faces(rgb)
-    except Exception:
-         faces = []
-    if len(faces) > 0:
-        for face in faces:
-            if face['confidence'] > 0.9:
-                x, y, w, h = face['box']
-                cv.rectangle(flippedFrame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        
 
-    cv.imshow("Live Camera Feed", flippedFrame)
+    frame = cv.flip(frame, 1)
+
+    faces = detector.detectFaces(frame)
+    frame = detector.drawFaces(frame, faces)
+
+    cv.imshow("Face Detection", frame)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv.destroyAllWindows()
-    
-    
---
+++ FaceDetector.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+from mtcnn import MTCNN
+
+class FaceDetector:
+    def __init__(self):
+        self.detector = MTCNN()
+    
+    def detectFaces(self, frame):
+        rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+        try:
+            faces = self.detector.detect_faces(rgb)
+        except Exception:
+            return []
+        return faces
+    def drawFaces(self, frame, faces):
+        for face in faces:
+            x, y, w, h = face['box']
+            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        return frame
